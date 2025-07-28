9231bf917525b4f7fe24110ffc3e45d3dac3a388
PaulaSamy5 <paulasamy52@gmial.com>
2025-07-28 19:05:59 +0300

    New updates

--- GraduationProject_E-ParkingSystem/obj/Debug/net8.0/GraduationProject_E-ParkingSystem.AssemblyInfo.cs
+++ GraduationProject_E-ParkingSystem/obj/Debug/net8.0/GraduationProject_E-ParkingSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GraduationProject_E-ParkingSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e5ec33ae0dee8bde8808b4d1908d725dfca8914")]
 [assembly: System.Reflection.AssemblyProductAttribute("GraduationProject_E-ParkingSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GraduationProject_E-ParkingSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
